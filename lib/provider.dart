50bd36cbfd188d73e9be9de2ea9249435d0aae5e
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-03-25 20:03:48 +0530

    added cart provider

---
+++ lib/provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shopping_cart/product_model.dart';
+import 'package:shopping_cart/service.dart';
+
+final productProvider = FutureProvider<List<Product>>((ref) async {
+  return ProductService().fetchProducts();
+});
+
+class CartNotifier extends StateNotifier<List<Product>> {
+  CartNotifier() : super([]);
+
+  void addToCart(Product product) {
+    state = state.map((p) {
+      if (p.id == product.id) {
+        return p.copyWith(quantity: p.quantity + 1); // Increase quantity if already in cart
+      }
+      return p;
+    }).toList();
+
+    if (!state.any((p) => p.id == product.id)) {
+      state = [...state, product.copyWith(quantity: 1)];
+    }
+  }
+
+  void increaseQuantity(Product product) {
+    state = state.map((p) {
+      if (p.id == product.id) {
+        return p.copyWith(quantity: p.quantity + 1);
+      }
+      return p;
+    }).toList();
+  }
+
+  void decreaseQuantity(Product product) {
+    state = state
+        .map((p) => p.id == product.id ? p.copyWith(quantity: p.quantity - 1) : p)
+        .where((p) => p.quantity > 0) // Remove products with 0 quantity
+        .toList();
+  }
+
+  double get totalPrice => state.fold(
+        0,
+        (sum, item) => sum + (item.discountedPrice * item.quantity),
+      );
+}
+
+final cartProvider = StateNotifierProvider<CartNotifier, List<Product>>((ref) => CartNotifier());
